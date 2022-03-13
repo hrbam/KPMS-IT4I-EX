d074c4de97e73d6e257f5d031dc708ed2383a996
hrbam <martin_hrba@post.cz>
2022-03-13 22:00:00 +0100

    row for 4 added

--- code/rf_karolina_pbs.sh
+++ code/rf_karolina_pbs.sh
@@ -14,6 +14,7 @@ echo "loaded R"
 time Rscript rf_serial.r
 time Rscript rf_mc.r 1
 time Rscript rf_mc.r 2
+time Rscript rf_mc.r 4
 time Rscript rf_mc.r 8
 time Rscript rf_mc.r 16
 time Rscript rf_mc.r 32
